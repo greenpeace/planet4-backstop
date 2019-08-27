b30df7ab04dd774b685e1b4b4a798bc4222fd5b8
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-08-27 16:13:45 +0300

    Simpler directory for checked out NRO code

--- mergescenarios.sh
+++ mergescenarios.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -f /src/$CIRCLE_PROJECT_REPONAME/backstop-pages.json ]; then
-  jq -s '.[0].scenarios = [.[].scenarios | add] | .[0]' backstop.json $CIRCLE_PROJECT_REPONAME/backstop-pages.json > combined.json
+if [ -f /src/repo/backstop-pages.json ]; then
+  jq -s '.[0].scenarios = [.[].scenarios | add] | .[0]' backstop.json /src/repo/backstop-pages.json > combined.json
 
   mv backstop.json backstop-backup.json
   mv combined.json backstop.json
