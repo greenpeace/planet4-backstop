8c063b1f0ac2e261901dff0e43f65764c3f54db9
Kiriakos Diamantis <replicator31@hotmail.com>
2019-09-13 11:53:05 +0300

    Modify function to check if images are loaded before proceeding

--- backstop_data/engine_scripts/planet4/onReady.js
+++ backstop_data/engine_scripts/planet4/onReady.js
@@ -10,5 +10,20 @@ module.exports = async (page, scenario, vp) => {
             window.jQuery('.carousel').carousel('pause');
         }
     });
-    // await page.waitFor(10000);
+
+    // 1. Find img tags.
+    // 2. Create a Promise which is resolved until all images fire load event.
+    await page.evaluate(async () => {
+        const imgs = Array.from(document.querySelectorAll("img"));
+
+        if (imgs.length > 0) {
+            await Promise.all(imgs.map(img => {
+                if (img.complete) return;
+                return new Promise((resolve, reject) => {
+                    img.addEventListener('load', resolve);
+                    img.addEventListener('error', reject);
+                });
+            }));
+        }
+    });
 };
