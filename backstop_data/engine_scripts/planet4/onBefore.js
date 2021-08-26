255b58a61cfb014ee4d573e729e94c33fb479d46
Florent Hernandez <florent.hernandez@greenpeace.org>
2021-08-26 09:28:48 +0200

    Fix onReady timeout on happypoint iframe
    We currently hide iframes in onReady.js, after the onReady event.
    This event is sometimes not triggered, when a happypoint is present on the page
    Removing iframes during loading seems to fix the issue.

--- backstop_data/engine_scripts/planet4/onBefore.js
+++ backstop_data/engine_scripts/planet4/onBefore.js
@@ -2,6 +2,19 @@ module.exports = async function (page, scenario, vp) {
 
     console.log('-------onBefore script-------');
 
+    page.on('load', () => {
+        console.log('-----Page loading-----');
+
+        // Remove iframes from the page
+        // to avoid happypoint issues
+        page.$$eval('iframe', (frames) => {
+            frames.map((frame) => {
+                console.log('Removing iframe');
+                frame.parentNode.removeChild(frame);
+            });
+        });
+    });
+
     page.once('load', () => {
         console.log('-----Page loaded!-----');
     });
