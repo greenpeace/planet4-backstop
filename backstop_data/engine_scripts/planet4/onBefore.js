dd1e5bcf2810d14c8a127076844d5180a45d4be9
Nikos Roussos <nikos@roussos.cc>
2022-02-18 11:54:15 +0200

    Revert "Add HappyPoint check"

--- backstop_data/engine_scripts/planet4/onBefore.js
+++ backstop_data/engine_scripts/planet4/onBefore.js
@@ -2,21 +2,6 @@ module.exports = async function (page, scenario, vp) {
 
     console.log('-------onBefore script-------');
 
-    page.on('load', () => {
-        console.log('-----Page loading-----');
-
-        // Remove happypoint from the page
-        // to avoid timeout issues
-        const happy = await page.evaluate(async () => {
-            const happy_point = document.getElementById('happy-point');
-            if (typeof (happy_point) != 'undefined' && happy_point != null) {
-                console.log('Removing happypoint');
-                happy_point.parentNode.removeChild(happy_point);
-            }
-        });
-        console.log(happy);
-    });
-
     page.once('load', () => {
         console.log('-----Page loaded!-----');
     });
