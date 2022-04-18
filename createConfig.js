aec0a805686b42a712b7b80aa72fd79a4cc6a85e
Nikos Roussos <nikos.roussos@greenpeace.org>
2022-04-18 14:36:05 +0300

    Simplify backstop scenarios
    - Move common config variables on global config
    - Adjust viewports based on our breakpoints
    - Add .editorconfig

--- createConfig.js
+++ createConfig.js
@@ -3,29 +3,27 @@ const fs = require('fs');
 const baseConfig = JSON.parse(fs.readFileSync('backstop.json'));
 let localConfig;
 try {
-    localConfig = JSON.parse(fs.readFileSync('/src/repo/backstop-pages.json'));
+  localConfig = JSON.parse(fs.readFileSync('/src/repo/backstop-pages.json'));
 } catch (e) {
-    localConfig = {};
+  localConfig = {};
 }
 
 const addQueryString = scenario => {
-    const url = scenario.url.includes('?') ? scenario.url : `${scenario.url}?backstop=1`;
+  const url = scenario.url.includes('?') ? scenario.url : `${scenario.url}?backstop=1`;
 
-    return {...scenario, url};
+  return {...scenario, url};
 };
 
 newConfig = {
-    ...baseConfig,
-    scenarios: [
-        ...baseConfig.scenarios,
-        ...(localConfig.scenarios || []),
-    ].map(addQueryString),
-    viewports: [
-        ...baseConfig.viewports,
-        ...(localConfig.viewports || []),
-    ],
+  ...baseConfig,
+  scenarios: [
+    ...baseConfig.scenarios,
+    ...(localConfig.scenarios || []),
+  ].map(addQueryString),
+  viewports: [
+    ...baseConfig.viewports,
+    ...(localConfig.viewports || []),
+  ],
 };
 
 fs.writeFileSync('backstop.json', JSON.stringify(newConfig, null, 2));
-
-
